1ffd689874b3b3e45036655210f862041198a9b5
Supriyo Rudra <117836404+rudrasro@users.noreply.github.com>
2023-01-23 13:25:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 //your code here
 function majorityElement(arr) {
-    let majority = nums[0], count = 1;
-    for (let i = 1; i < nums.length; i++) {
-        if (count === 0) {
-            majority = nums[i];
-            count = 1;
-        } else if (majority === nums[i]) {
-            count++;
-        } else {
-            count--;
-        }
-    }
-    return majority;
+    arr.sort((a, b) => a - b);
+
+  const l = arr.length;
+
+  if (l % 2 === 0) {
+    return arr[l / 2]
+  } else {
+    return arr[(l - 1) / 2]
+  }
 }
